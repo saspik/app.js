6af2cd21b7495beca958ef44cbf7b4121dfc1b7e
saspik <80147380+saspik@users.noreply.github.com>
2021-03-06 11:36:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function getNumber (num){
+
+    for(let i=1; i<a+1; i++){
+        if (i==5 )
+        console.log('пять')
+        else if (i==13)
+        console.log('тринадцать')
+        else if (i==22)
+        console.log('двадцать два')
+        else if (i==35)
+        console.log('тридцать пять')
+        else if (i==98)
+        console.log('девяносто восемь')
+        else
+        console.log(i)
+    }
+
+}
+getNumber(a=100)
